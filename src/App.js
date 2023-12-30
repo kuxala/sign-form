e6534f182632fc4afea82de1aa18005d4e8ce194
kuxala <cyberdna12@gmail.com>
2023-12-30 12:59:15 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="wrapper">
+      <div className="left-col">
+        <h1>Learn to code by watching others</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="right-col ">
+        <div className="plans">
+          <p>Try it free 7 days then $20/mo. thereafter</p>
+        </div>
+        <div className="container">
+          <div className="inputs">
+            <input placeholder="First Name" />
+            <input placeholder="Last Name" />
+            <input placeholder="Email Address" />
+            <input placeholder="Password" />
+          </div>
+          <button>
+            <a href="#">CLAIM YOUR FREE TRIAL</a>
+          </button>
+          <p className="p">
+            By clicking the button, you are agreeing to our
+            <span> Terms and Services</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
